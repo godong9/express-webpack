b87a9c479f2e0e7c99138949b05e3c147b623e06
Donghyun Go <godong9@gmail.com>
2016-11-13 19:52:45 +0900

    Update version

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,18 +7,22 @@ module.exports = yeoman.Base.extend({
   prompting: function () {
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the badass ' + chalk.red('generator-express-webpack') + ' generator!'
+      'Welcome to the ' + chalk.red('express-webpack') + ' generator!'
     ));
 
     var prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
+      type: 'input',
+      name: 'userAnswer',
+      message: 'Would you like make ./server in here?(y/N)',
+      default: 'y'
     }];
 
     return this.prompt(prompts).then(function (props) {
       // To access props later use this.props.someAnswer;
+      if (props.userAnswer === 'N') {
+        process.exit(1);
+        return;
+      }
       this.props = props;
     }.bind(this));
   },
